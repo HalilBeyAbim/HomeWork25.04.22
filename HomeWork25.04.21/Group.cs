07be7594d4943c897669a825a946631ffcdd9369
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-23 20:59:09 +0400

    added Home Work 25.04.22

---
+++ HomeWork25.04.21/Group.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork25._04._21
+{
+    class Group
+    {
+        public string No;
+        public byte Limit;
+        private Student[] _students;
+        public Group(string number, byte limit)
+        {
+            No = number;
+            Limit = limit;
+            _students = new Student[0];
+        }
+        public void AddStudent(Student student)
+        {
+            if (Limit > _students.Length)
+            {
+                Array.Resize(ref _students, _students.Length + 1);
+                _students[_students.Length - 1] = student;
+                Console.WriteLine("yarandi");
+            }
+            else
+            {
+                Console.WriteLine("Yer yoxdur");
+            }
+        }
+        public void GetStudents()
+        {
+            foreach (var students in _students)
+            {
+                Console.WriteLine(students.Name, students.Surname);
+            }
+        }
+
+    }
+}
---
+++ HomeWork25.04.21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork25._04._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Group group = new Group("P324",2);    
+
+            Student student1 = new Student("Xelilbey","Xelilbeyli");
+            Student student2 = new Student("Memmedbagir", "Bagirzade");
+            Student student3 = new Student("Siyavus", "Aslan");
+
+            group.AddStudent(student1);
+            group.AddStudent(student2);
+            group.AddStudent(student3);
+            group.GetStudents();
+
+
+        }
+    }
+}
